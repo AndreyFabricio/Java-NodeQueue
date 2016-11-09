7181bbaef8dfb55c28608b7730e29c24efe06001
Andrey Fabricio <andrey.fas@gmail.com>
2016-11-09 17:34:21 -0300

    Create IFila.java

---
+++ IFila.java
@@ -0,0 +1,19 @@
+package fila;
+
+public interface IFila {
+	
+	public void limpar();
+	
+	public int tamanho();
+	
+	public boolean estaVazia();
+
+	public boolean contem(int elemento);
+	
+	public int distancia(int elemento);
+	
+	public int remover();
+
+	public void enfileirar(int elemento);
+	
+}//Fim da Interface IFila
